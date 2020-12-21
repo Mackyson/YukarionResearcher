c55dc567efc0ce747b9f6fb17d8058250755f67b
Mackyson <mackyson72@gmail.com>
2020-12-22 01:33:59 +0900

    はい．

---
+++ go.mod
@@ -0,0 +1,5 @@
+module YukarionWords
+
+go 1.15
+
+require github.com/bluele/mecab-golang v0.0.0-20180831023624-c8cfe04e87f9 // indirect
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/bluele/mecab-golang"
+)
+
+var count map[string]int
+
+func getWords() []string {
+	count = make(map[string]int)
+	var words []string
+
+	tmp, _ := ioutil.ReadAll(os.Stdin)
+	words = strings.Split(string(tmp), ",")
+	return words
+}
+
+func parseToNode(m *mecab.MeCab) {
+	tg, err := m.NewTagger()
+	if err != nil {
+		panic(err)
+	}
+	defer tg.Destroy()
+
+	yukarionWords := getWords()
+
+	for _, word := range yukarionWords {
+		lt, err := m.NewLattice(word)
+		if err != nil {
+			panic(err)
+		}
+		defer lt.Destroy()
+
+		node := tg.ParseToNode(lt)
+		for {
+			features := strings.Split(node.Feature(), ",")
+			if features[0] == "連体詞" {
+				count[node.Surface()] += 1
+			}
+			if node.Next() != nil {
+				break
+			}
+		}
+	}
+}
+
+func main() {
+	m, err := mecab.New("-d /usr/lib/mecab/dic/mecab-ipadic-neologd")
+	if err != nil {
+		panic(err)
+	}
+	defer m.Destroy()
+	parseToNode(m)
+
+	for str, cnt := range count {
+		if cnt >= 2 {
+			fmt.Printf("%s,%d\n", str, cnt)
+		}
+	}
+}
